2720e5bdf4aac73ca51ab7d8f7c678316d5dd3fc
kiruxaspb <65371121+kiruxaspb@users.noreply.github.com>
2022-01-20 20:02:46 +0300

    Update setupchain.sh

--- network/setupchain.sh
+++ network/setupchain.sh
@@ -9,10 +9,12 @@ wasp-cli set wasp.0.nanomsg 127.0.0.1:5550
 wasp-cli set wasp.0.peering 127.0.0.1:4000
 wasp-cli peering info
 wasp-cli peering list-trusted
+echo "input your pubkey:"
 read pubkey
 wasp-cli peering trust $varname 127.0.0.1:4000
 wasp-cli peering list-trusted
 wasp-cli request-funds
 wasp-cli chain deploy --committee=0 --quorum=1 --chain=mychain --description="IOTACHAIN"
+echo "input your wallet:"
 read wallet
 asp-cli chain evm deploy -a mychain --alloc $wallet:1000000000000000000000000
