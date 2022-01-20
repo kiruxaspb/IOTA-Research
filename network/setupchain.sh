e509699388d7998194c5cddba79745fee5d12190
kiruxaspb <65371121+kiruxaspb@users.noreply.github.com>
2022-01-20 20:01:33 +0300

    Create setupchain.sh

---
+++ network/setupchain.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "--------Setup chain...---------"
+echo "---------------------------------"
+cd /wasp
+wasp-cli init
+wasp-cli set goshimmer.api 127.0.0.1:8080
+wasp-cli set wasp.0.api 127.0.0.1:9090
+wasp-cli set wasp.0.nanomsg 127.0.0.1:5550
+wasp-cli set wasp.0.peering 127.0.0.1:4000
+wasp-cli peering info
+wasp-cli peering list-trusted
+read pubkey
+wasp-cli peering trust $varname 127.0.0.1:4000
+wasp-cli peering list-trusted
+wasp-cli request-funds
+wasp-cli chain deploy --committee=0 --quorum=1 --chain=mychain --description="IOTACHAIN"
+read wallet
+asp-cli chain evm deploy -a mychain --alloc $wallet:1000000000000000000000000
