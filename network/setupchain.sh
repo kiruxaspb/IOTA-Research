7e692cf00d37ee30f77f11956d0d8679d0b859d5
kiruxaspb <65371121+kiruxaspb@users.noreply.github.com>
2022-01-21 17:17:44 +0300

    Update setupchain.sh

--- network/setupchain.sh
+++ network/setupchain.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 echo "--------Setup chain...---------"
 echo "---------------------------------"
+echo "Input number node:"
+read nnode
 cd /wasp
 wasp-cli init
 wasp-cli set goshimmer.api 127.0.0.1:8080
-wasp-cli set wasp.0.api 127.0.0.1:9090
-wasp-cli set wasp.0.nanomsg 127.0.0.1:5550
-wasp-cli set wasp.0.peering 127.0.0.1:4000
+wasp-cli set wasp.$nnode.api 127.0.0.1:9090
+wasp-cli set wasp.$nnode.nanomsg 127.0.0.1:5550
+wasp-cli set wasp.$nnode.peering 127.0.0.1:4000
 wasp-cli peering info
 wasp-cli peering list-trusted
-echo "input your pubkey:"
+echo "Input pubkey for peering:"
 read pubkey
 wasp-cli peering trust $varname 127.0.0.1:4000
 wasp-cli peering list-trusted
 wasp-cli request-funds
 wasp-cli chain deploy --committee=0 --quorum=1 --chain=mychain --description="IOTACHAIN"
-echo "input your wallet:"
+echo "Input wallet address:"
 read wallet
 wasp-cli chain evm deploy -a mychain --alloc $wallet:1000000000000000000000000
 sleep 5
